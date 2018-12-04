c389adcec81cbab92668765167186e3f43a4e215
code_xiaoxuesheng <996945978@qq.com>
2018-12-04 10:46:38 +0800

    init project

---
+++ src/main/java/com/zzh/springboot_tmall/SpringbootTmallApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.springboot_tmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTmallApplication.class, args);
+	}
+}
